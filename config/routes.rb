662258e40ecdde763de60017fcb47f3c4a819a12
adityapandit17 <adityapandit38@gmail.com>
2019-02-20 12:03:37 +0530

    added admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :articles
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :articles
+  end 
 end
