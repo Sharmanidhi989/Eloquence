2d699c6a4d47c47020624adbe84c371203bd1363
adityapandit17 <adityapandit38@gmail.com>
2019-02-20 15:30:21 +0530

    added validation to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
    mount_uploader :image, ImageUploader
+   validates :title, presence: true,
+                    length: { minimum: 5 }
+   validates :description, presence: true                 
 end
