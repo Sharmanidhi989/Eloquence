72948dfc88b8a8dadf448584006b08353a0a1c5b
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-02-21 16:58:28 +0530

    added comment controller and changed routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
    validates :title, presence: true,
                     length: { minimum: 5 }
    validates :description, presence: true
-   has_many :comments                 
+   has_many :comments, dependent: :destroy              
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   resources :articles
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   namespace :admin do
     resources :articles
   end 
+
+  resources :articles do
+    resources :comments
+  end
 end
