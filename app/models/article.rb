78d3f1a26ffda81b2e01330c5e3ca80a49b91b84
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-02-21 16:17:40 +0530

    added comment model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
    mount_uploader :image, ImageUploader
    validates :title, presence: true,
                     length: { minimum: 5 }
-   validates :description, presence: true                 
+   validates :description, presence: true
+   has_many :comments                 
 end
