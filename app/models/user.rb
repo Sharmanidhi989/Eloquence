230a1d743319fca27a000a01cdd3722a8c573aad
nkroker <nikhil.tyagi@sait.ac.in>
2019-02-22 18:40:48 +0530

    Added username and email login feature in user schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,35 @@
 class User < ApplicationRecord
+
+	validates :username, presence: :true, uniqueness: { case_sensitive: false }
+	validate :validate_username
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, password_length: 8..128
+         :recoverable, :rememberable, :validatable, password_length: 8..128,
+          authentication_keys: [:login]
   has_many :comments
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+
+	def validate_username
+	  if User.where(email: username).exists?
+	    errors.add(:username, :invalid)
+	  end
+	end
+
+
+  def self.find_for_database_authentication(warden_conditions)
+	  conditions = warden_conditions.dup
+	  if login = conditions.delete(:login)
+	    where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	  elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+	    where(conditions.to_h).first
+	  end
+	end
+
 end
