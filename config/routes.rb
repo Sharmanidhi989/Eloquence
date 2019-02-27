42aa0b746d0c6194096f171dff5a783ef1e8405d
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-02-27 17:08:33 +0530

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   devise_for :users
-  resources :articles
+  resources :articles, only: [:index, :show]
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/pages/:page" => "pages#show"  
