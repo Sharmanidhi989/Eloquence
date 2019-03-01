01dd0703c87e926680c18c8fd31669cdc8e73e35
nkroker <nikhil.tyagi@sait.ac.in>
2019-03-01 14:11:02 +0530

    image preview added in edit user form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,14 @@
 //= require ckeditor/init
 //= require activestorage
 //= require_tree .
+function readURL(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('#preview').attr('src', e.target.result);
+		}
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
